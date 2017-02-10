69d7dc3e1c8dc59d95def42c85759f96ad07f1a5
Will Dunwoody <will.dunwoody@gmail.com>
2017-02-10 11:55:33 +0100

    Have webpage working, just need to clean up java

--- javaetch.js
+++ javaetch.js
@@ -1,18 +1,38 @@
-$(document).ready(function(){
-  createPad(45);
-});
 
 
-function = createPad(x){
+function createPad(x){
   for (var rows = 0; rows < x; rows++){
     for (var cols = 0; cols < x; cols++){
       $('#container').append('<div class = "grid"></div>');
+      $('.grid').height(600/x);
+      $('.grid').width(600/x);
+      $('.grid').css('float', 'left');
     };
   };
-  $('.grid').width(600/x);
-  $('.grid').height(600/x);
 };
 
-function = clearPad (){
+function clearPad (){
   $('.grid').remove();
 };
+
+function sketch(){
+  $('.grid').mouseenter(function(){
+    $(this).css('background-color', '#000')
+  });
+};
+
+function userChoice(){
+  var y = prompt ("How big would you like your grid to be?");
+  clearPad();
+  createPad(y);
+};
+
+$(document).ready(function(){
+  x = 16;
+  createPad(x);
+  sketch();
+  $('input').click(function(){
+    userChoice();
+    sketch();
+  });
+});
